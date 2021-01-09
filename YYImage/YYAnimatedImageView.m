9d761e47111ad9d6ebe00eb42795605a9035f907
H了个朮 <45927787+hlgc@users.noreply.github.com>
2021-01-09 15:31:17 +0800

    iOS 14+ UIImageView
    iOS 14+ UIImageView

--- YYImage/YYAnimatedImageView.m
+++ YYImage/YYAnimatedImageView.m
@@ -528,6 +528,11 @@ typedef NS_ENUM(NSUInteger, YYAnimatedImageType) {
 - (void)displayLayer:(CALayer *)layer {
     if (_curFrame) {
         layer.contents = (__bridge id)_curFrame.CGImage;
+    } else {
+        // iOS 14+ UIImageView use this delegate method for rendering.
+        if ([UIImageView instancesRespondToSelector:@selector(displayLayer:)]) {
+            [super displayLayer:layer];
+        }
     }
 }
 
